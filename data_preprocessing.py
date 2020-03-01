00ac737e5324796c55796e9a1390f955022a589b
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-03-01 18:20:20 -0500

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -18,6 +18,7 @@ missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbos
 missingvalues = missingvalues.fit(X[:, 1:3])
 X[:, 1:3]=missingvalues.transform(X[:, 1:3])
 
+
 #Encoding Categorical variables (Country and Purchased column)into numbers 
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
@@ -27,7 +28,13 @@ X = np.array(ct.fit_transform(X), dtype=np.float)
 from sklearn.preprocessing import LabelEncoder
 y = LabelEncoder().fit_transform(y)
 
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X =StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
